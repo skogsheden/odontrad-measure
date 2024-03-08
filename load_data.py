588d5dd75b5a2439e42a426c27386c66294d174b
Nils Gustafsson <ngustafsson90@gmail.com>
2024-03-08 22:09:56 +0100

    bugfix

--- load_data.py
+++ load_data.py
@@ -6,9 +6,14 @@ from PIL import Image, ImageTk, ImageDraw
 
 def load_images_from_folder(self):
     folder_path = filedialog.askdirectory()
-    image_files = [f for f in os.listdir(folder_path) if
-                   os.path.isfile(os.path.join(folder_path, f)) and f.lower().endswith(
-                       ('.jpg', '.jpeg', '.png', '.tiff'))]
+    if not folder_path:  # Check if user cancels the dialog
+        return
+
+    folder_path = os.path.normpath(folder_path)  # Normalizing path
+
+    image_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))
+                   and f.lower().endswith(('.jpg', '.jpeg', '.png', '.tiff'))]
+
     self.image_list = [os.path.join(folder_path, file_name) for file_name in image_files]
     self.image_current_id = 0
 
@@ -22,7 +27,6 @@ def open_image_from_list(self, index):
                 self.canvas.delete(last_rectangle)  # Delete the rectangle from the canvas
                 self.canvas.delete(last_text)
             self.rectangles.clear()
-
         self.image_filepath = self.image_list[index]
         original_image = Image.open(self.image_filepath)
         screen_q = self.canvas.winfo_screenwidth() / self.canvas.winfo_screenheight()
@@ -30,7 +34,6 @@ def open_image_from_list(self, index):
         img_height = original_image.height
         img_width = original_image.width
         img_q = img_width / img_height
-        print(screen_q, img_q)
         if img_q > screen_q:
             screen_width = self.canvas.winfo_screenwidth() - 100
             screen_height = round(screen_width / img_q)
@@ -44,7 +47,7 @@ def open_image_from_list(self, index):
         self.canvas.config(width=screen_width, height=screen_height)
         self.canvas_image = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo_image)
         self.draw = ImageDraw.Draw(self.image)
-        filename_parts = self.image_filepath.split('/')
+        filename_parts = self.image_filepath.split(os.path.sep)
         self.image_filename = filename_parts[-1]  # Sista delen är filnamnet, hela path behövs ej
 
         # Calculate scale conversion
@@ -60,6 +63,7 @@ def open_image_from_list(self, index):
 
 def open_image(self):
     file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png;*.tiff")])
+
     if self.rectangles:
         for rectangle in self.rectangles:
             last_rectangle = rectangle["rect"]
@@ -89,7 +93,7 @@ def open_image(self):
         self.canvas.config(width=screen_width, height=screen_height)
         self.canvas_image = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.photo_image)
         self.draw = ImageDraw.Draw(self.image)
-        filename_parts = file_path.split('/')
+        filename_parts = self.image_filepath.split('/')
         self.image_filename = filename_parts[-1]  # Sista delen är filnamnet, hela path behövs ej
         print("Bild laddad")
 
